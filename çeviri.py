41a56aba9ef18fa05af59789735ee1bdd7028917
mert <mertcankul06@hotmail.com>
2020-08-11 15:55:51 +0300

    bazı hatalar duzenlendi

--- çeviri.py
+++ çeviri.py
@@ -2,9 +2,10 @@ from googletrans import Translator
 cevir = ""
 while cevir != "exit":
     translator = Translator() 
-    cevir1 = input("çevirmek istediğiniz kelime(çıkmak için exit yaz):")
-    if cevir1 == "exit":
+    cevir3 = input("hangi dil çevrilecek(çıkmak için exit yaz):")
+    if cevir3 == "exit":
         break
+    cevir1 = input("çevirmek istediğiniz kelime:")
     cevir2 = input("hangi dile çevirilecek:")
-    a = translator.translate(cevir1,dest=cevir2)
+    a = translator.translate(src=cevir3,text=cevir1,dest=cevir2)
     print(a.text)
